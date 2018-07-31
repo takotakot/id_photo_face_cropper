00158213645f0c22efd4c291d351c68864abfdad
Takuho NAKANO <takotakot@users.noreply.github.com>
2018-07-31 12:16:57 +0900

    get o2

--- face_cropper.cpp
+++ face_cropper.cpp
@@ -13,7 +13,8 @@
 // using namespace dlib;
 // using namespace std;
 
-class face_cropper {
+class face_cropper
+{
     dlib::frontal_face_detector detector;
     dlib::shape_predictor predictor;
     std::vector<dlib::rectangle> faces;
@@ -27,7 +28,8 @@ class face_cropper {
         dlib::deserialize("shape_predictor_68_face_landmarks.dat") >> predictor;
     }
 
-    template<typename T> void detect(dlib::cv_image<T> &image)
+    template <typename T>
+    void detect(dlib::cv_image<T> &image)
     {
         faces.clear();
         faces = detector(image);
@@ -38,12 +40,14 @@ class face_cropper {
             shapes.push_back(predictor(image, *it));
         }
     }
-	
-	int get_num_faces() {
-		return faces.size();
-	}
 
-    cv::Mat get_points_index(int index[], const int &n_index, dlib::full_object_detection &shape) {
+    int get_num_faces()
+    {
+        return faces.size();
+    }
+
+    cv::Mat get_points_index(int index[], const int &n_index, dlib::full_object_detection &shape)
+    {
         cv::Mat point_matrix(cv::Size(2, n_index), CV_64FC1);
         for (int i = 0; i < n_index; ++i)
         {
@@ -72,44 +76,59 @@ class face_cropper {
         return get_points_index(eye_points_index, n_eye_points, shape);
     }
 
-	void crop_nth(cv::Mat &i_img, int n, cv::Mat &o_img) {
+    void crop_nth(cv::Mat &i_img, int n, cv::Mat &o_img)
+    {
         cv::Mat center_points = get_center_points(shapes[n]);
         cv::Mat eye_points = get_eye_points(shapes[n]);
+
+        cv::Mat uc;
+        cv::reduce(center_points, uc, 0, CV_REDUCE_AVG);
+        cv::PCA pca_center_points(center_points, cv::Mat(), CV_PCA_DATA_AS_ROW, 2);
+        cv::Mat c0 = pca_center_points.eigenvectors.row(0);
+        cv::Mat c1 = pca_center_points.eigenvectors.row(1);
+
+        cv::Mat ue;
+        cv::reduce(eye_points, ue, 0, CV_REDUCE_AVG);
+
+        cv::Mat mo2 = (ue - uc) * c0.t() * c0 + uc;
     }
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
-    // thread_local 
+    // thread_local
     face_cropper cropper;
     char buffer[5];
 
     //text on screen
     std::ostringstream outtext;
-	std::cerr << __LINE__ << std::endl;
+    std::cerr << __LINE__ << std::endl;
 
     //main loop
     do
     {
         // Grab a frame
         cv::Mat temp, o_img;
-    	cv::Mat cropped;
+        cv::Mat cropped;
         std::string filename;
         // cap >> temp;
         if (argc < 2)
         {
             temp = cv::imread("IMG_4899.jpg");
-        }else{
-        	temp = cv::imread(argv[1]);
+        }
+        else
+        {
+            temp = cv::imread(argv[1]);
         }
         dlib::cv_image<dlib::bgr_pixel> cimg(temp);
 
         cropper.detect(cimg);
 
-    	std::cerr << __LINE__ << std::endl;
+        std::cerr << __LINE__ << std::endl;
 
-    	for(int i = 0; i < cropper.get_num_faces(); ++i) {
+        for (int i = 0; i < cropper.get_num_faces(); ++i)
+        {
             std::cerr << __LINE__ << std::endl;
             cropper.crop_nth(temp, i, o_img);
             std::snprintf(buffer, sizeof(buffer), "_%d", i);
@@ -119,8 +138,7 @@ int main(int argc, char* argv[])
             cv::imwrite(filename.c_str(), o_img);
         }
         std::cerr << __LINE__ << std::endl;
-    }while (false);
-
+    } while (false);
 
     return 0;
 }
