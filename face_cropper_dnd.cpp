7d55364b2806f37818aa7dd82aebb87765720c62
Takuho NAKANO <takotakot@users.noreply.github.com>
2018-08-30 11:33:55 +0900

    Move function create_all

--- face_cropper_dnd.cpp
+++ face_cropper_dnd.cpp
@@ -1,5 +1,6 @@
 #include "face_cropper.h"
 #include "read_srcs.h"
+#include "functions.h"
 #include <ctime>
 #include<sys/stat.h>
 
@@ -10,44 +11,6 @@ struct face_detect_result
     int rw, rh;
     int detected; // 0|1
 };
-
-void create_all(std::string date_suffix, struct src_set sst)
-{
-    std::string dest_dirname;
-    for (auto src : sst.srcs)
-    {
-        if(src.type == 2) {
-            dest_dirname = src.name + "/../" + src.name + date_suffix + "/";
-            // std::cerr << "mkdir: " << dest_dirname << std::endl;
-            if (mkdir(dest_dirname.c_str(),
-                          S_IRUSR | S_IWUSR | S_IXUSR | /* rwx */
-                          S_IRGRP | S_IWGRP | S_IXGRP | /* rwx */
-                          S_IROTH | S_IXOTH | S_IXOTH)  /* rwx */
-             == 0)
-            {
-                std::string sub_dirname;
-                for (auto dir : src.dir.dirlist)
-                {
-                    sub_dirname = dest_dirname + dir;
-                    // std::cerr << "mkdir: " << sub_dirname << std::endl;
-                    if (mkdir(sub_dirname.c_str(),
-                              S_IRUSR | S_IWUSR | S_IXUSR |     /* rwx */
-                                  S_IRGRP | S_IWGRP | S_IXGRP | /* rwx */
-                                  S_IROTH | S_IXOTH | S_IXOTH)  /* rwx */
-                        == 0)
-                    {
-                        // OK
-                    }else
-                    {
-                        std::cerr << "create error" << std::endl;
-                    }
-                }
-            }else{
-                std::cerr << "create error" << std::endl;
-            }
-        }
-    }
-}
 
 int main(int argc, char *argv[])
 {
