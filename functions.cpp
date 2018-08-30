77b082f444f65aa817593cc2ccde6cfad1f93143
Takuho NAKANO <takotakot@users.noreply.github.com>
2018-08-30 11:33:55 +0900

    Move function create_all

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,43 @@
-#include"functions.h"
+#include "functions.h"
+
+void create_all(std::string date_suffix, struct src_set sst)
+{
+    std::string dest_dirname;
+    for (auto src : sst.srcs)
+    {
+        if (src.type == 2)
+        {
+            dest_dirname = src.name + "/../" + src.name + date_suffix + "/";
+            // std::cerr << "mkdir: " << dest_dirname << std::endl;
+            if (mkdir(dest_dirname.c_str(),
+                      S_IRUSR | S_IWUSR | S_IXUSR |     /* rwx */
+                          S_IRGRP | S_IWGRP | S_IXGRP | /* rwx */
+                          S_IROTH | S_IXOTH | S_IXOTH)  /* rwx */
+                == 0)
+            {
+                std::string sub_dirname;
+                for (auto dir : src.dir.dirlist)
+                {
+                    sub_dirname = dest_dirname + dir;
+                    // std::cerr << "mkdir: " << sub_dirname << std::endl;
+                    if (mkdir(sub_dirname.c_str(),
+                              S_IRUSR | S_IWUSR | S_IXUSR |     /* rwx */
+                                  S_IRGRP | S_IWGRP | S_IXGRP | /* rwx */
+                                  S_IROTH | S_IXOTH | S_IXOTH)  /* rwx */
+                        == 0)
+                    {
+                        // OK
+                    }
+                    else
+                    {
+                        std::cerr << "create error" << std::endl;
+                    }
+                }
+            }
+            else
+            {
+                std::cerr << "create error" << std::endl;
+            }
+        }
+    }
+}
--- functions.h
+++ functions.h
@@ -1,4 +1,7 @@
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
+#include "read_srcs.h"
+
+void create_all(std::string date_suffix, struct src_set sst);
 
 #endif // FUNCTIONS_H_
